5778001153ecf4dd4a0ed8817203119848cf501e
Michael Levy <mike.levy.work@gmail.com>
2019-11-18 14:49:53 -0700

    Starting work on recreating Moore table
    Table 1 from Moore et al (2013)... we'll be updating it to use CESM2 data.

--- get_ocn_cmip5_files.sh
+++ get_ocn_cmip5_files.sh
@@ -2,19 +2,29 @@
 
 # Scipt by klindsay, this is adaptation of
 # https://github.com/klindsay28/CESM2_coup_carb_cycle_JAMES/blob/master/get_atm_cmip5_files.sh
-for case in b40.prescribed_carb.001 \
-            b40.1850_ramp.1deg.ncbdrd.001 \
-            b40.1850_ramp.1deg.ncbdrc.001 \
-            b40.1850_ramp.1deg.ncbcrd.002 \
-            b40.20th.1deg.bdrd.001 \
-            b40.coup_carb.004 \
-            b40.20th.1deg.coup.001 ; do
+
+if [ $# -ne 1 ]; then
+  echo "ERROR: this script requires a single argument"
+  echo "Usage: $0 VAR_NAME"
+  exit 1
+fi
+
+VAR_NAME=$1
+
+#for case in b40.prescribed_carb.001 \
+#            b40.1850_ramp.1deg.ncbdrd.001 \
+#            b40.1850_ramp.1deg.ncbdrc.001 \
+#            b40.1850_ramp.1deg.ncbcrd.002 \
+#            b40.20th.1deg.bdrd.001 \
+#            b40.coup_carb.004 \
+#            b40.20th.1deg.coup.001 ; do
+for case in b40.20th.1deg.bdrd.001 ; do
 
    echo $case
    cd /glade/p/cgd/oce/projects/cesm2-marbl/intake-esm-data/$case/ocn/proc/tseries/monthly
    pwd
-   hsi "cd /CCSM/csm/$case/ocn/proc/tseries/monthly ; ls *IRON_FLUX*"
-   hsi "cd /CCSM/csm/$case/ocn/proc/tseries/monthly ; cget *IRON_FLUX*"
+   hsi "cd /CCSM/csm/$case/ocn/proc/tseries/monthly ; ls *${VAR_NAME}*"
+   hsi "cd /CCSM/csm/$case/ocn/proc/tseries/monthly ; cget *${VAR_NAME}*"
 
 done
 
