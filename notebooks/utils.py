a95b42d188b011415c0cf6ae5301c7d6e286fa60
Michael Levy <mike.levy.work@gmail.com>
2019-12-23 13:25:16 -0700

    More flexibility in zonal mean
    User can specify a grid to use a pre-determined grid file (currently only for
    gx1v7) if dataset has been stripped of some necessary variables such as ULAT
    and ULONG.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,11 +3,19 @@ import subprocess
 import tempfile
 import xarray as xr
 
-def zonal_mean_via_fortran(ds, var):
-    ds_in_file = tempfile.NamedTemporaryFile()
-    ds_out_file = tempfile.NamedTemporaryFile()
+def zonal_mean_via_fortran(ds, var, grid=None):
+    """
+    Write ds to a temporary netCDF file, compute zonal mean for
+    a given variable based on Keith L's fortran program, read
+    resulting netcdf file, and return the new xarray dataset
+    """
+
+    # xarray doesn't require the ".nc" suffix, but it's useful to know what the file is for
+    ds_in_file = tempfile.NamedTemporaryFile(suffix='.nc')
+    ds_out_file = tempfile.NamedTemporaryFile(suffix='.nc')
     ds.to_netcdf(ds_in_file.name)
-    ds.to_netcdf('/glade/scratch/mlevy/xarray_tonc.nc')
+
+    # Set up location of the zonal average executable
     za_exe = os.path.join(os.path.sep,
                           'glade',
                           'u',
@@ -16,28 +24,50 @@ def zonal_mean_via_fortran(ds, var):
                           'bin',
                           'zon_avg',
                           'za')
-    grid_file = os.path.join(os.path.sep,
-                             'glade',
-                             'collections',
-                             'cdg',
-                             'timeseries-cmip6',
-                             'b.e21.B1850.f09_g17.CMIP6-piControl.001',
-                             'ocn',
-                             'proc',
-                             'tseries',
-                             'month_1',
-                             'b.e21.B1850.f09_g17.CMIP6-piControl.001.pop.h.NO3.000101-009912.nc')
+    if grid:
+        # Point to a file that contains all necessary grid variables
+        # I think that is KMT, TLAT, TLONG, TAREA, ULAT, ULONG, UAREA, and REGION_MASK
+        if grid == 'gx1v7':
+            grid_file = os.path.join(os.path.sep,
+                                     'glade',
+                                     'collections',
+                                     'cdg',
+                                     'timeseries-cmip6',
+                                     'b.e21.B1850.f09_g17.CMIP6-piControl.001',
+                                     'ocn',
+                                     'proc',
+                                     'tseries',
+                                     'month_1',
+                                     'b.e21.B1850.f09_g17.CMIP6-piControl.001.pop.h.NO3.000101-009912.nc')
+        else:
+            print(f'WARNING: no grid file for {grid}, using xarray dataset for grid vars')
+            grid_file = ds_in_file.name
+    else:
+        # Assume xarray dataset contains all needed fields
+        grid_file = ds_in_file.name
+
+    # Set up the call to za with correct options
     za_call = [za_exe,
                '-v', var,
                '-grid_file', grid_file,
                '-kmt_file', grid_file,
                '-O', '-o', ds_out_file.name, # -O overwrites existing file, -o gives file name
                ds_in_file.name]
+
+    # Use subprocess to call za, allows us to capture stdout and print it
     proc = subprocess.Popen(za_call, stdout=subprocess.PIPE)
     (out, err) = proc.communicate()
-    print(out.decode('utf-8'))
-    ds_out = xr.open_dataset(ds_out_file.name)
+    if not out:
+        # Read in the newly-generated file
+        print('za ran successfully, writing netcdf output')
+        ds_out = xr.open_dataset(ds_out_file.name)
+    else:
+        print(f'za reported an error:\n{out.decode("utf-8")}')
+
+    # Delete the temporary files and return the new xarray dataset
     ds_in_file.close()
     ds_out_file.close()
-    return(ds_out)
+    if not out:
+        return(ds_out)
+    return(None)
 
