a652bf30c33bd46117687f4090dda73b0c71c46a
Michael Levy <mike.levy.work@gmail.com>
2020-02-10 15:55:11 -0700

    Refactort nutrient plots notebook
    add climo_utils.py to hold functions that will also be used in chl plots

---
+++ notebooks/climo_utils.py
@@ -0,0 +1,223 @@
+def read_CESM_var(dq, time_slice, variable):
+    # Define datasets
+    dataset = dq['ocn.historical.pop.h']
+
+    keep_vars = ['REGION_MASK', 'z_t', 'dz', 'TAREA', 'TLONG', 'TLAT', 'time', 'time_bound', 'member_id', 'ctrl_member_id'] + [variable]
+    dataset = dataset.drop([v for v in dataset.variables if v not in keep_vars]).sel(time=time_slice).mean(dim=['member_id', 'time']).compute()
+
+    return(dataset)
+
+##################################################
+
+def read_obs(src, variable=None):
+    import os
+    import xarray as xr
+
+    if src not in ['WOA', 'SeaWiFS']:
+        raise ValueError(f'{src} is not a valid source')
+
+    xr_kwargs = dict()
+    if src == 'WOA':
+        # varmap is used to define the filename
+        varmap = {'NO3' : 'n', 'PO4' : 'p', 'SiO3' : 'i'}
+        if variable not in varmap:
+            raise ValueError(f'{variable} is not a valid variable for WOA')
+        root_dir=os.path.join(os.path.sep, 'glade', 'p', 'cgd', 'oce', 'projects',
+                              'cesm2-marbl', 'woa2018-data', 'POP_gx1v7', 'annual')
+        filename=f'woa18_all_{varmap[variable]}00_gx1v7.nc'
+        xr_kwargs['decode_times'] = False
+
+    if src == 'SeaWiFS':
+        root_dir=os.path.join(os.path.sep, 'glade', 'p', 'cgd', 'oce', 'projects',
+                              'cesm2-marbl', 'seaWIFS-data')
+        filename='seaWIFS.chl_gsm.mon_climo.Sep1997_Dec2010.nc'
+
+    # Read in dataset, use first time dim from WOA
+    ds=xr.open_dataset(os.path.join(root_dir, filename), **xr_kwargs)
+    if src == 'WOA':
+        ds = ds.isel(time=0)
+    return(ds)
+
+##################################################
+
+def plot_surface_vals(variable, ds, da, obs):
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import BoundaryNorm
+    from matplotlib.ticker import MaxNLocator
+
+    import cartopy
+    import cartopy.crs as ccrs
+
+    TLONG = ds[variable].TLONG
+    TLAT = ds[variable].TLAT
+    computed = da[variable]
+    observed = obs[variable]
+    bias=computed-observed # convert to /vol rather than /mass
+
+    # Determine contours
+    min_lev = 0
+    if variable == 'NO3':
+        max_lev = 42
+    if variable == 'PO4':
+        max_lev = 3.2
+    if variable == 'SiO3':
+        max_lev = 180
+    levels = MaxNLocator(nbins=15).tick_values(min_lev, max_lev)
+    cmap = plt.get_cmap('rainbow')
+    norm = BoundaryNorm(levels, ncolors=cmap.N)
+
+    fig = plt.figure(figsize=(8, 14))
+    ax = plt.subplot(3, 1, 1, projection=ccrs.Robinson(central_longitude=305.0))
+
+    pc = ax.pcolormesh(TLONG,
+                       TLAT,
+                       computed.to('mmol/m^3').magnitude,
+                       cmap=cmap,
+                       transform=ccrs.PlateCarree(),
+                       norm=norm)
+
+    # ax.add_feature(cartopy.feature.NaturalEarthFeature('physical','land','110m',
+    #                                                    edgecolor='face',
+    #                                                    facecolor='lightgray'))
+    ax.set_global()
+    ax.coastlines(linewidth=0.5)
+    cb = plt.colorbar(pc, shrink=0.6)
+    ax.set_title(f'{variable} from POP run');
+    cb.set_label('$\mathrm{mmol/m^3}$')
+
+    ax = plt.subplot(3, 1, 2, projection=ccrs.Robinson(central_longitude=305.0))
+
+    pc = ax.pcolormesh(TLONG,
+                       TLAT,
+                       observed.to('mmol/m^3').magnitude,
+                       cmap=cmap,
+                       transform=ccrs.PlateCarree(),
+                       norm=norm)
+
+    # ax.add_feature(cartopy.feature.NaturalEarthFeature('physical','land','110m',
+    #                                                    edgecolor='face',
+    #                                                    facecolor='lightgray'))
+    ax.set_global()
+    ax.coastlines(linewidth=0.5)
+    cb = plt.colorbar(pc, shrink=0.6)
+    ax.set_title(f'{variable} from WOA');
+    cb.set_label('$\mathrm{mmol/m^3}$')
+
+    ax = plt.subplot(3, 1, 3, projection=ccrs.Robinson(central_longitude=305.0))
+
+    pc = ax.pcolormesh(TLONG,
+                       TLAT,
+                       bias.to('mmol/m^3').magnitude,
+                       cmap=plt.get_cmap('bwr'),
+                       transform=ccrs.PlateCarree())
+
+    # ax.add_feature(cartopy.feature.NaturalEarthFeature('physical','land','110m',
+    #                                                    edgecolor='face',
+    #                                                    facecolor='lightgray'))
+    ax.set_global()
+    ax.coastlines(linewidth=0.5)
+    cb = plt.colorbar(pc, shrink=0.6)
+    ax.set_title(f'Bias in {variable}');
+    cb.set_label('$\mathrm{mmol/m^3}$')
+
+##################################################
+
+def plot_global_profile(variables, units, ds, da, obs):
+    import matplotlib.pyplot as plt
+
+    plt_cnt = len(variables)
+    z = ds[variables[0]].z_t.data * units[ds[variables[0]]['z_t'].attrs['units']]
+
+    fig = plt.figure(figsize=(4*plt_cnt, 4))
+    for n, variable in enumerate(variables):
+        computed = da[variable]
+        observed = obs[variable]
+
+        ax = fig.add_subplot(1, plt_cnt, n+1)
+
+        ax.plot(computed.magnitude, (z.to('m')).magnitude, 'b-', label='CESM2', linewidth=2)
+        ax.plot(observed.magnitude, (z.to('m')).magnitude, 'r:', label='WOA2018', linewidth=2)
+
+        ax.set_title(f'Global Profile of {variable}')
+        ax.set(xlabel='concentration (mmol m$^{-3}$)')
+        if n == 0:
+            ax.set(ylabel='depth (m)')
+        else:
+            ax.set_yticklabels('')
+        plt.gca().invert_yaxis()
+        ax.legend()
+
+##################################################
+
+def return_magnitude_in_units(pint_obj, units):
+    return((pint_obj.to('mmol/m^3')).magnitude)
+
+##################################################
+
+def plot_zonal_averages_by_region(variables, region, da, obs, lat, z):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    if type(variables) != list:
+        variables = [variables]
+
+    # For now use levels defined in Kristen's notebook
+    levels = dict()
+    levels['NO3'] = np.arange(0, 48, 4) # 0, 4, 8, ..., 40, 44
+    levels['PO4'] = np.arange(0, 4.4, 0.4) # 0, 0.4, 0.8, ..., 3.6, 4.0
+    levels['SiO3'] = np.concatenate((np.arange(0, 100, 10), np.arange(100, 300, 20))) # 0, 10, ... 90, 100, 120, 140, ..., 260, 280
+
+    bias_levels = dict()
+    bias_levels['NO3'] = np.arange(-18, 20, 3) # -18, -15, ..., 15, 18
+    bias_levels['PO4'] = np.arange(-0.9, 1, 0.1) # -0.9, -0.8, ..., 0.8, 0.9
+    bias_levels['SiO3'] = np.arange(-80, 85, 5) # -80, -75, -70, ..., 75, 80
+
+    fig = plt.figure(figsize=(8*len(variables),12))
+    plt.suptitle(f'Zonal Means ({region})', fontsize=14)
+
+    # TOP: CESM
+
+    for n, variable in enumerate(variables):
+        cesm_out = return_magnitude_in_units(da[region][variable], 'mmol/m^3')
+        woa_vals = return_magnitude_in_units(obs[region][variable], 'mmol/m^3')
+        bias = cesm_out - woa_vals
+
+        ax = fig.add_subplot(3, len(variables), n + 1)
+        ax.set_title(f' {variable}\nCESM2')
+        pc=ax.contourf(lat, z, cesm_out, levels=levels[variable], cmap='rainbow', extend='both')
+        pc2 = ax.contour(lat, z, cesm_out, levels[variable], colors='k')
+        ax.clabel(pc2, colors = 'k', fmt = '%2.1f', fontsize=10)
+        if n==0:
+            ax.set(ylabel='depth (m)')
+        else:
+            ax.set(ylabel='', yticklabels='')
+        ax.set(xlabel='', xticklabels='')
+        ax.invert_yaxis()
+
+        #MIDDLE: WOA
+
+        ax = fig.add_subplot(3, len(variables), (n + 1) + len(variables))
+        ax.set_title(f'WOA')
+        pc=ax.contourf(lat, z, woa_vals, levels=levels[variable], cmap='rainbow', extend='both')
+        pc2 = ax.contour(lat, z, woa_vals, levels[variable], colors='k')
+        ax.clabel(pc2, colors = 'k', fmt = '%2.1f', fontsize=10)
+        if n==0:
+            ax.set(ylabel='depth (m)')
+        else:
+            ax.set(ylabel='', yticklabels='')
+        ax.set(xlabel='', xticklabels='')
+        ax.invert_yaxis()
+
+        #BOTTOM: Bias
+
+        ax = fig.add_subplot(3, len(variables), (n + 1) + 2*len(variables))
+        ax.set_title(f'CESM2 - WOA')
+        pc=ax.contourf(lat, z, bias, levels=bias_levels[variable], cmap='bwr', extend='both')
+        pc2 = ax.contour(lat, z, bias, bias_levels[variable], colors='k')
+        ax.clabel(pc2, colors = 'k', fmt = '%2.1f', fontsize=10)
+        if n==0:
+            ax.set(ylabel='depth (m)')
+        else:
+            ax.set(ylabel='', yticklabels='')
+        ax.set(xlabel='Latitude')
+        ax.invert_yaxis()
